2d5204112267afabefb99d02e5930cda41a9af96
yifeng002 <493788939lizhaogai@163.com>
2017-05-25 23:16:27 +0800

    update

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const Tenant = require('./lib/Tenant');
 const Account = require('./lib/Account');
 const Auth = require('./lib/Auth');
 const Space = require('./lib/Space');
+const Task = require('./lib/Task');
+const TaskStatus = require('./lib/TaskStatus');
+const Dolink = require('./lib/Dolink');
 /**
  * 
  *
@@ -45,6 +48,9 @@ module.exports = function(baseUrl, opts) {
   client.account = new Account(requestor);
   client.auth = new Auth(requestor);
   client.space = new Space(requestor);
+  client.task = new Task(requestor);
+  client.taskStatus = new TaskStatus(requestor);
+  client.dolink = new Dolink(requestor);
 
   return client;
 };
